889ca679bfdb083c877b61df181fca8d57dea388
Rashad Kokash <richardeo112@gmail.com>
2020-11-20 19:44:50 +0200

    Updates

--- sw.js
+++ sw.js
@@ -32,7 +32,7 @@ self.__precacheManifest = [
     "url": "commons.2f2933dc343b474e635f.css"
   },
   {
-    "url": "commons-276d7252dfc5486b9415.js"
+    "url": "commons-ad16d52116823c313619.js"
   },
   {
     "url": "app-cb889b82e7d3a94fe2e0.js"
@@ -42,7 +42,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "4c8ce8e7fe43599629426408cb2f48ab"
+    "revision": "3e0e39d0f0e93a2a435e341ae4685f4a"
   },
   {
     "url": "manifest.webmanifest",
